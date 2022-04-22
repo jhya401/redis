5e398c299d2b8be9b6d075ba43720f33cfdf75fb
lee.juhee <lee.juhee@kstm.co.kr>
2022-04-22 11:56:20 +0900

    파일위치 정리

--- src/main/java/com/example/redis/config/RedissonClientConfig.java
+++ src/main/java/com/example/redis/config/RedissonClientConfig.java
@@ -1,10 +1,9 @@
-package com.example.redis;
+package com.example.redis.config;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/com/example/redis/utils/ElapsedTimeUtils.java
+++ src/main/java/com/example/redis/utils/ElapsedTimeUtils.java
@@ -1,4 +1,4 @@
-package com.example.redis;
+package com.example.redis.utils;
 
 import java.text.NumberFormat;
 import java.time.Duration;
